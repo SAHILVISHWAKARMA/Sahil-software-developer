16a68b717b8d497af72717296d3b5bc21f5f5a6a
avnishy84 <avnishy84@gmail.com>
2026-04-04 21:07:05 +0530

    init

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST="{}"
+self.__REACT_LOADABLE_MANIFEST="{\"components\\\\ui\\\\ContactForm.tsx -> @emailjs/browser\":{\"id\":\"components\\\\ui\\\\ContactForm.tsx -> @emailjs/browser\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_emailjs_browser_es_index_js.js\"]}}"
