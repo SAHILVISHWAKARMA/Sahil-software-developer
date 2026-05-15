4b2cfeea408212afef890e2c7cb2f9764049c817
avnishy84 <avnishy84@gmail.com>
2026-05-15 09:40:08 +0000

    new

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,13 +1,13 @@
 import { initializeApp, getApps } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDkKlLT-Ucj2A-Rw3CANy5O56q_USFl_Fs',
-  authDomain: 'sahil-vishwakarma.firebaseapp.com',
-  projectId: 'sahil-vishwakarma',
-  storageBucket: 'sahil-vishwakarma.firebasestorage.app',
-  messagingSenderId: '502537642856',
-  appId: '1:502537642856:web:2e7d88e4eaa8091a752cdd',
-  measurementId: 'G-WMN9KS3FF4',
+  apiKey: 'AIzaSyBcYMc4yYJSfpwTerh8LdCOKVAW6Kf9RXQ',
+  authDomain: 'sahil-vishwakarma-50e27.firebaseapp.com',
+  projectId: 'sahil-vishwakarma-50e27',
+  storageBucket: 'sahil-vishwakarma-50e27.firebasestorage.app',
+  messagingSenderId: '1001664083108',
+  appId: '1:1001664083108:web:dde307829ece7956202231',
+  measurementId: 'G-5CXV2EY074',
 };
 
 // Prevent duplicate initialization (important for Next.js)
